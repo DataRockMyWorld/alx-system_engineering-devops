7610b2d9fc82159da846bf6b131e94645a73c798
Jewel Bansah <jewelbansah@rocketmail.com>
2024-03-06 09:56:17 +0000

    refix all tasks

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,14 +6,9 @@ import requests
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-            "User-Agent": "faithokoth"
-            }
-    params = {
-            "limit": 10
-            }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    headers = {"User-Agent": "faithokoth"}
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
