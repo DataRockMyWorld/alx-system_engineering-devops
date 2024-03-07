7ffc47db65a8b3f3985b4e0ae65efffb444ddff0
Jewel <jewelbansah@rocketmail.com>
2024-03-07 13:38:14 -0800

    fix task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,10 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {"User-agent": 'python:subreddit.subscriber.counter:v1.0 (by /u/juelzgh)'}
+    ua = 'python:subreddit.subscriber.counter:v1.0 (by /u/juelzgh)'
+    user_agent = {
+        "User-agent": ua
+    }
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = get(url, headers=user_agent)
     results = response.json()
